eda910bb173c72d310e41720d1b5d431fb92899c
kenkennie <kennedy.mut7@gmail.com>
2022-05-22 04:19:41 +0300

    session

--- cart/cart.py
+++ cart/cart.py
@@ -18,15 +18,41 @@ class Cart():
         # get the created session and save in crt
 
 
-    def add(self, product):
+    def add(self, product,Qty):
         '''
             add data to session 
-            product is passed from view
+            product,product_Qty is passed from view
         '''
         product_id=product.id
         # get product.id(from product) and save to product_id
         if product_id not in self.basket:
             # check if product id exists in session.crt
-            self.basket[product_id] = {'price':int(product.price)}
+            self.basket[product_id] = {'price':int(product.price), 'Qty':int(Qty)}
             # if it doen't exist, add price
         self.sess.modified= True
+
+    def __init__ (self):
+        '''
+        collect the product_id in the session data to query the database and 
+        return products
+        '''
+        product_ids = self.crt.keys()
+        # collect product id that are in session and 
+        products = Product.object.filter(id__in = product_ids)
+        # get product from db, and filter products id in table with the ones ids in session
+        cart = self.cart.copy()
+        # copy data from sesssion data
+        for  product in product:
+            # loop through data from database
+            # each product that is looped it is saved in variable product
+            cart[str(product.id)]['product']= prdcts 
+            
+            
+            # add moreda
+    
+    def __len__(self):
+        '''
+        get the basket data and count the quantity of all products
+        '''
+        return sum(prdct['Qty'] for prdct in self.basket.values())
+        # iterate over the product quantity,calculate values of all products tha have quantity
