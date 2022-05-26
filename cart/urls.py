0ec1f3c66254f07eb66ec08203fc907c1ce5212e
kenkennie <kennedy.mut7@gmail.com>
2022-05-26 05:52:28 +0300

    test delete

--- cart/urls.py
+++ cart/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.cart_summary, name='cart_summary'),
     path('cart_add/', views.cart_add, name='cart_add'),
     path('cart_delete/', views.cart_delete, name='cart_delete'),
-    path('cart_summary', views.cart_summary, name='cart_summary'),
+    path('cart_summary', views.cart_summary,  name='cart_summary'),
 
 ]
