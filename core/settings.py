93e7212847127ee5ffa7702b054339969ccf5dc2
kenkennie <42668750+kenkennie@users.noreply.github.com>
2022-06-30 09:06:58 +0000

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,6 +25,22 @@ SECRET_KEY = 'django-insecure-&4k+hh_+xxqzkeaen-b-z58o5^8!p7lp!s4mq%^*f^)sxz(o$%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 ALLOWED_HOSTS = ['kennie-mega.herokuapp.com','12.0.0.1']
 
 # Application definition
