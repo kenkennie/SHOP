4aa88745afcb32a89e40f85a7feaf2f0d431f7e5
kenkennie <42668750+kenkennie@users.noreply.github.com>
2022-06-30 10:48:03 +0000

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&4k+hh_+xxqzkeaen-b-z58o5^8!p7lp!s4mq%^*f^)sxz(o$%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['kennie-mega.herokuapp.com','the-mega-shop.herokuapp.com','127.0.0.1']
